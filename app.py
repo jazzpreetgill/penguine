444cf0ad188bd4903f6e700e58bc98836606ce1a
jazzpreetgill <140133292+jazzpreetgill@users.noreply.github.com>
2025-07-30 09:30:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,7 @@ with st.expander("Data"):
 with st.expander("Data Visualization"):
   st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color='species')
 
-with st.expander("Input data"):
-  pass
+
 
 with st.expander("Data Preperation"):
   pass
@@ -45,3 +44,20 @@ with st.sidebar:
   flipper_length_mm = st.slider('Flipper length (mm)',172.0,231.0,201.0)
   body_mass_g = st.slider('Body mass (g)',2700.0,6300.0,4207.0)
   gender = st.selectbox('Gender',('male','female'))
+
+  data = {'island':island,
+          'bill_length_mm':bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender': gender  
+  }
+  input_df = pd.DataFrame(data, index[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander("Input data"):
+  st.write("**Input data**")
+  input_df
+  st.write("**Combined data**")
+  input_penguins
+  
