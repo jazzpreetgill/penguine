b4095f46a43878a8cd877ccc9c1a25e03b5d2d69
jazzpreetgill <140133292+jazzpreetgill@users.noreply.github.com>
2025-07-30 09:48:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ with st.expander("Input data"):
   input_penguins
 # One hot encoding for X
 encode = ['island','sex']
-df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 # One hot encoding for y
